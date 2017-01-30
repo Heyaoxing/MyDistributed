692724617a096be6c7b4795531c8957cdf9a756e
730530507 <730530507@qq.com>
2017-01-30 12:33:57 +0800

    创建项目

---
+++ MyDistributed/MyDistributed.UnitTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDistributed.UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key1 = "WT20161212001";
+            Console.WriteLine(key1.GetHashCode());
+            Console.Read();
+        }
+    }
+}
